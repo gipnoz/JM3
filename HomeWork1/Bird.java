f0276eb3f68a9b55e6898a8bd72e06ac631ce509
gipnoz <levatelie@gmail.com>
2016-11-16 00:22:53 +0200

    иди на хер GIT

---
+++ HomeWork1/Bird.java
@@ -0,0 +1,31 @@
+package HomeWork1;
+
+
+public class Bird {
+    public static void main(String[] args) {
+        BirdJob bird = new BirdJob();
+        bird.sing();
+        bird.fly();
+        bird.walk();
+        bird.whoAmI();
+
+    }
+}
+class BirdJob {
+    void sing() {
+
+        System.out.println("I am singing");
+    }
+    void fly() {
+
+        System.out.println("I am flying");
+    }
+    void walk() {
+
+        System.out.println("I am walking");
+    }
+    void whoAmI() {
+        System.out.println("I am Bird");
+    }
+}
+
